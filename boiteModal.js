bb1544927eeb37c31636ceb41ac2f5025726e134
Nicolas Goyon <goyonnicolas33@gmail.com>
2024-10-07 11:05:00 +0200

    Dernier commit

--- boiteModal.js
+++ boiteModal.js
@@ -2,21 +2,13 @@
 function showModal(imageId) {
 let modalText = document.getElementById("modalText");
 if (imageId === 'image1') {
-    modalText.textContent = "Ce projet est effectué dans l'année de BTS portant sur la création d'un site de restauration.";
+    modalText.textContent = "Ce projet de groupe est effectué dans l'année de BTS portant sur la création d'un site de gestion des commandes pour des restaurants avec une fonction livraison. Actuellement entrain de produire le front end en travaillant avec HTML/CSS.";
 } else if (imageId === 'image2') {
-    modalText.textContent = "Ce projet est un portfolio créé en HTML/CSS/JS ";
+    modalText.textContent = "Ce projet est la première version d'un portfolio créé en HTML/CSS/JS dont l'objectif est de retransmettre un maximum d'information utile à des potentiels recruteurs.";
 }
 document.getElementById("myModal").style.display = "flex";
 }
 
 function closeModal() {
 document.getElementById("myModal").style.display = "none";
-}
-
-// Fermer la modale en cliquant en dehors
-window.onclick = function(event) {
-let modal = document.getElementById("myModal");
-if (event.target == modal) {
-    modal.style.display = "none";
-}
 }
